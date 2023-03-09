6c9dcacb52c1f646cdddf79e5a156d7a9bd45143
nxtdxve <zettlerd@bzz.ch>
2023-03-09 21:47:28 +0100

    changed the way to obtain the HWID

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import dns.resolver
 import pymongo
 import requests
 import wmi
+import hashlib
 
 import config
 
@@ -69,9 +70,37 @@ def check_user_authentication() -> None:
     db = client[db_name]
     users_collection = db["users"]
 
-    # Get the HWID of the computer
     wmi_obj = wmi.WMI()
-    hwid = wmi_obj.Win32_ComputerSystemProduct()[0].UUID
+
+    # Get processor ID
+    processor_id = wmi_obj.Win32_Processor()[0].ProcessorId.strip()
+
+    # Get motherboard ID
+    motherboard_id = wmi_obj.Win32_BaseBoard()[0].SerialNumber.strip()
+
+    # Get hard drive ID
+    hard_drive_id = ''
+    for drive in wmi_obj.Win32_DiskDrive():
+        if "fixed" in drive.MediaType.lower():
+            hard_drive_id = drive.SerialNumber.strip()
+            break
+
+    # Get network adapter ID
+    network_adapter_id = ''
+    for adapter in wmi_obj.Win32_NetworkAdapter():
+        if adapter.MACAddress is not None:
+            network_adapter_id = adapter.MACAddress.replace(':', '').strip()
+            break
+
+    # Combine the IDs to create a unique hardware ID
+    hwid_added = f"{processor_id}-{motherboard_id}-{hard_drive_id}-{network_adapter_id}"
+
+    # Hash the HWID to generate a 32-character string
+    hwid_hash = hashlib.sha256(hwid_added.encode()).hexdigest()
+
+    # Split the hash into four parts and join with hyphens
+    hwid_parts = [hwid_hash[i:i+8] for i in range(0, 32, 8)]
+    hwid = '-'.join(hwid_parts)
 
     user = users_collection.find_one({"hwid": hwid})
 
